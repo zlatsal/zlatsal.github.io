2f62de4c33e20ae23be17bca8d1a3f3475eae41e
Zlatan Salkic <54914295+zlatsal@users.noreply.github.com>
2023-02-28 13:30:45 +0100

    Add files via upload

---
+++ projects/text2words/script.js
@@ -0,0 +1,27 @@
+const textAreaGrab = document.getElementById("text-area");
+
+function grabSplit() {
+    textAreaText = textAreaGrab.value;
+    // clean all alphanumericals
+    let cleanAlpha = textAreaText.replace(/[^a-z0-9 ]/gi, "");
+
+    // all chars to lowercase
+    let lowerCase = cleanAlpha.toLowerCase();
+
+    // convert to array
+    let words = lowerCase.split(" ");
+
+    // clean duplicate values (in this case words in the sentence)
+    // note: learn more about Set
+    let cleanDuplicate = [...new Set(words)];
+    document.getElementById("result-words").innerHTML =
+        cleanDuplicate.join("<br>");
+}
+
+const button = document.getElementById("btn-split");
+
+function splitText() {}
+
+button.addEventListener("click", () => {
+    grabSplit();
+});
